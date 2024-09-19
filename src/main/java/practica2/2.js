21ca2665926ffb10e709d9e7c8b352a292bcae16
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2024-09-19 12:15:51 -0300

    01

---
+++ src/main/java/practica2/2.js
@@ -0,0 +1,14 @@
+//Impresión de elementos de matriz con delimitadores de coma
+
+function printArray(array) {
+ return array.join('')
+}
+
+
+let des = printArray(["h", "o", "l", "a"])
+console.log(des)
+
+
+
+
+
